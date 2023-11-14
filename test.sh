26b948866db21ee697b122261889c106a1c2dd89
Andrei Kashin <kashin.andrej@gmail.com>
2023-11-14 17:08:17 +0000

    Add direct compilation benchmark

--- test.sh
+++ test.sh
@@ -1,6 +1,10 @@
-sudo ../../Downloads/dist/cargo-clif b --release
-./target/release/cranelift-bench >no-wasm.txt
+# Direct compilation
+cargo run --release > results/rustc.txt
 
-sudo cargo build --target wasm32-wasi --release
+# Using rustc_cranelift
+dist/cargo-clif run --release > results/rustc_cranelift.txt
+
+# Using WASM cranelift
+cargo build --target wasm32-wasi --release
 wasmtime compile target/wasm32-wasi/release/cranelift-bench.wasm -o compiled-wasm
-wasmtime compiled-wasm --allow-precompiled >with-wasm.txt
+wasmtime compiled-wasm --allow-precompiled > results/wasm_cranelift.txt
