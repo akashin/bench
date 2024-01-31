81adc16a5be427a7611475672fb9a8787fd5c99f
Andrei Kashin <kashin.andrej@gmail.com>
2024-01-31 16:20:14 +0000

    Add wasmi benchmark

--- src/main.rs
+++ src/main.rs
@@ -3,30 +3,80 @@
 
 use panic_halt as _;
 
-use sha2::{Digest, Sha256};
-
 extern "C" {
-    fn assert_eq_i64(actual: u64, expected: u64);
+    fn assert_eq_i32(actual: i32, expected: i32);
+    fn allocate(size: usize);
 }
-// fn assert_eq_i64(actual: u64, expected: u64) {
+// fn assert_eq_i32(actual: i32, expected: i32) {
 //     assert_eq!(actual, expected);
 // }
 
-fn assert_eq_array(expected: &[u8], actual: &[u8]) {
-    for i in 0..32 {
+// use wasmi::*;
+
+use core::{
+    alloc::{GlobalAlloc, Layout},
+};
+
+struct BumpPointerAlloc;
+
+unsafe impl GlobalAlloc for BumpPointerAlloc {
+    unsafe fn alloc(&self, layout: Layout) -> *mut u8 {
         unsafe {
-            assert_eq_i64(expected[i] as u64, actual[i] as u64);
+            allocate(layout.size());
         }
+        return core::ptr::null_mut();
+        // sys_alloc_aligned(layout.size(), layout.align())
+    }
+
+    unsafe fn dealloc(&self, _: *mut u8, _: Layout) {
+        // this allocator never deallocates memory
     }
 }
 
-#[no_mangle]
-pub fn main() {
-    let hash = Sha256::digest(b"X");
-    let expected = [
-        75, 104, 171, 56, 71, 254, 218, 125, 108, 98, 193, 251, 203, 238, 191, 163, 94, 171, 115,
-        81, 237, 94, 120, 244, 221, 173, 234, 93, 246, 75, 128, 21,
-    ];
+#[global_allocator]
+static HEAP: BumpPointerAlloc = BumpPointerAlloc;
 
-    assert_eq_array(&expected, &hash)
+#[no_mangle]
+fn main() {
+    // First step is to create the Wasm execution engine with some config.
+    // In this example we are using the default configuration.
+    let engine = wasmi::Engine::default();
+    // let wat = r#"
+    //     (module
+    //         (import "host" "hello" (func $host_hello (param i32)))
+    //         (func (export "hello")
+    //             (call $host_hello (i32.const 3))
+    //         )
+    //     )
+    // "#;
+    // Wasmi does not yet support parsing `.wat` so we have to convert
+    // out `.wat` into `.wasm` before we compile and validate it.
+    // let wasm = wat::parse_str(&wat).unwrap();
+    let wasm = include_bytes!("../add.wasm");
+    let module = wasmi::Module::new(&engine, &wasm[..]).unwrap();
+
+    // All Wasm objects operate within the context of a `Store`.
+    // Each `Store` has a type parameter to store host-specific data,
+    // which in this case we are using `42` for.
+    type HostState = ();
+    let mut store = wasmi::Store::new(&engine, ());
+    let host_assert = wasmi::Func::wrap(
+        &mut store,
+        |_caller: wasmi::Caller<'_, HostState>, lhs: i32, rhs: i32| {
+            unsafe {
+                assert_eq_i32(lhs, rhs);
+            }
+        },
+    );
+
+    // In order to create Wasm module instances and link their imports
+    // and exports we require a `Linker`.
+    let mut linker = <wasmi::Linker<HostState>>::new(&engine);
+    // Instantiation of a Wasm module requires defining its imports and then
+    // afterwards we can fetch exports by name, as well as asserting the
+    // type signature of the function with `get_typed_func`.
+    //
+    // Also before using an instance created this way we need to start it.
+    linker.define("env", "assert_eq", host_assert).unwrap();
+    let _ = linker.instantiate(&mut store, &module).unwrap().start(&mut store).unwrap();
 }
