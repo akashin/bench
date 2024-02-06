d27b73573cb6cb82c2cdfe4b4d0afbb79832fe0e
Andrei Kashin <kashin.andrej@gmail.com>
2024-02-06 14:44:57 +0000

    Implement keccak

--- src/compute.py
+++ src/compute.py
@@ -1,4 +1,5 @@
 import hashlib
+from Crypto.Hash import keccak
 
 def subslice_to_u64(s, begin, end):
     result = 0
@@ -9,8 +10,12 @@ def subslice_to_u64(s, begin, end):
 
 # s = (b"X" * 32 * 14)
 s = b"X"
-h = hashlib.sha256(s)
-d = h.digest()
+# h = hashlib.keccak256(s)
+k = keccak.new(digest_bits=256)
+k.update(s)
+# h = hashlib.sha256(s)
+d = k.digest()
+# d = h.digest()
 
 print(d.hex())
 print([int(v) for v in d])
--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@
 
 use panic_halt as _;
 
-use sha2::{Digest, Sha256};
+use sha3::{Digest, Keccak256};
 
 extern "C" {
     fn assert_eq_i64(actual: u64, expected: u64);
@@ -22,10 +22,10 @@ fn assert_eq_array(expected: &[u8], actual: &[u8]) {
 
 #[no_mangle]
 pub fn main() {
-    let hash = Sha256::digest(b"X");
+    let hash = Keccak256::digest(b"X");
     let expected = [
-        75, 104, 171, 56, 71, 254, 218, 125, 108, 98, 193, 251, 203, 238, 191, 163, 94, 171, 115,
-        81, 237, 94, 120, 244, 221, 173, 234, 93, 246, 75, 128, 21,
+        85, 12, 100, 161, 80, 49, 195, 6, 68, 84, 193, 154, 220, 98, 67, 166, 18, 44, 19, 138, 36,
+        46, 170, 9, 141, 165, 11, 177, 20, 252, 141, 86,
     ];
 
     assert_eq_array(&expected, &hash)
